52bc00412386a09bb41c12cb13c2406994ed67c9
Satyam Revgade <satyamrevgadelinkedin@gmail.com>
2025-12-25 11:53:27 +0530

    started with frontend with shadcn.ui

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from './components/ui/components_lite/Navbar'
+const App = () => {
+  return (
+    <div>
+        <Navbar/>
+    </div>
+  )
+}
+
+export default App
