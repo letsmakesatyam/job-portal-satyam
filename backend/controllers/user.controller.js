39da030d9bec75ef4362751d364aca1e790ed9e7
Satyam Revgade <satyamrevgadelinkedin@gmail.com>
2026-01-09 15:00:19 +0530

    updated token info for cross platform

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -148,7 +148,7 @@ export const login = async (req, res) => {
       .cookie("token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "Strict",
+        sameSite: "none",
         maxAge: 24 * 60 * 60 * 1000,
       })
       .status(200)
--- frontend/src/utils/data.js
+++ frontend/src/utils/data.js
@@ -1,4 +1,4 @@
-export const USER_API_ENDPOINT="http://localhost:5001/api/users"
-export const APPLICATION_API_ENDPOINT="http://localhost:5001/api/application"
-export const JOB_API_ENDPOINT="http://localhost:5001/api/job"
-export const COMPANY_API_ENDPOINT="http://localhost:5001/api/company"
+export const USER_API_ENDPOINT="https://job-portal-satyam.onrender.com/api/users"
+export const APPLICATION_API_ENDPOINT="https://job-portal-satyam.onrender.com/api/application"
+export const JOB_API_ENDPOINT="https://job-portal-satyam.onrender.com/api/job"
+export const COMPANY_API_ENDPOINT="https://job-portal-satyam.onrender.com/api/company"
