0dc8438cc4bdcde1551fb30562a4a8ef707ed61f
Satyam Revgade <satyamrevgadelinkedin@gmail.com>
2026-01-10 08:46:08 +0530

    debugging /me endpoint

--- frontend/src/hooks/checkUserLoggedIn.jsx
+++ frontend/src/hooks/checkUserLoggedIn.jsx
@@ -6,20 +6,30 @@ import { USER_API_ENDPOINT } from "../utils/data.js";
 import { setUser } from '../redux/authSlice.js'
 
 // Example logic for your hook
+// hooks/checkUserLoggedIn.js
 const checkUserLoggedIn = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     const fetchUser = async () => {
       try {
-        // Your API call here
-        const res = await axios.get('/api/users/me' , {withCredentials: true,});
-        dispatch(setUser(res.data.user));
+        const res = await axios.get('https://job-portal-satyam-x06l.onrender.com/api/users/me', {
+          withCredentials: true,
+        });
+        
+        console.log("Auth Response Data:", res.data); // DEBUGGING
+
+        if (res.data.success) {
+          dispatch(setUser(res.data.user));
+        } else {
+          dispatch(setUser(null));
+        }
       } catch (err) {
-        dispatch(setUser(null)); // This should set isLoading to false
+        console.error("Auth Hook Error:", err.response?.data || err.message);
+        dispatch(setUser(null));
       }
     };
     fetchUser();
-  }, []);
+  }, [dispatch]);
 };
 
 export default checkUserLoggedIn;
